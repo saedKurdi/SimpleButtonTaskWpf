29b66181ff1bd2245ac257c03e622f0452b8ad84
= <=>
2023-10-17 16:58:16 +0400

    SimpleWPFtask

---
+++ WpfTask1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTask1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_right_down(object sender, RoutedEventArgs e)
+        {
+            Button ?btn = sender as Button;
+            main_grid.Children.Remove(btn);
+            main_window.Title = btn!.Content.ToString();
+        }
+
+        private void button_left_down(object sender, RoutedEventArgs e)
+        {
+            Button? btn = sender as Button;
+            byte r = (byte)Random.Shared.Next(0, 256);
+            byte g = (byte)Random.Shared.Next(0, 256);
+            byte b = (byte)Random.Shared.Next(0, 256);
+
+            btn!.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+            MessageBox.Show($"Content:{btn.Content}\nHeight:{btn.Height}\nWidth:{btn.Width}");
+        }
+    }
+}
